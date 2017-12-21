31d614139c1543155dd939b1b844336c2d6c4234
Tautvydas Dulskis <tautvydas@dulskis.eu>
2017-12-21 19:00:15 +0200

    8 paskaitos php kodas

--- submit.php
+++ submit.php
@@ -5,4 +5,17 @@
  * Date: 17.12.14
  * Time: 18.33
  */
-echo "Labas pasauli";
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8mb4', 'root', 'root');
+
+    $sql_query = "INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES (1, 'Erichsen', 'Skagen', 'Stavanger', 'Berlin')";
+
+    $result = $db->exec($sql_query);
+    $insertId = $db->lastInsertId();
+
+    echo "Last inserted element ID: " . $insertId;
+
+} catch (\PDOException $e) {
+    echo "Gavom klaida: " . $e->getMessage();
+}
