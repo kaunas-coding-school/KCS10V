ef7ea962040a3fa845b04c44b46e68a5614fccd4
minsol <sologubas5>
2017-12-21 20:51:14 +0200

    zinute

--- Mindaugas/JavaScript/testas.js
+++ Mindaugas/JavaScript/testas.js
@@ -33,3 +33,17 @@ var keistiTaba = function (elementas) {
     $(sheetId).show().addClass('active_sheet');
 
 }
+
+$('.submit8tn').click(function () {
+
+    var duomenys = {};
+    duomenys["vardas"] = $('[name="vardas"]').val();
+    duomenys["pavarde"] = $('[name="pavarde"]').val();
+    duomenys["pastas"] = $('[name="pastas"]').val();
+    duomenys["zinute"] = $('[name="zinute"]').val();
+
+    $.post("/Mindaugas/connect.php", duomenys, function ( data ){
+        $('.Aatsakymas').html(data);
+    });
+
+});
